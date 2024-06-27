6d491f64c2a9081572f128f66e5160e3a9437c47
masumcse1 <optinfra.masum@gmail.com>
2024-06-27 17:09:47 +0600

    test

--- src/test/java/com/opt/repository/CustomerRepositoryTest.java
+++ src/test/java/com/opt/repository/CustomerRepositoryTest.java
@@ -4,13 +4,13 @@ import com.opt.entity.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@SpringBootTest
-@DataJpaTest
+@SpringBootTest
 class CustomerRepositoryTest {
 
     @Autowired
